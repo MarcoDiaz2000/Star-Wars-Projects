7f9ee504e52f2d6f6c09afbe89ad6b70b79fedd3
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-05-22 04:31:33 -0600

    Display list of items on the Homepage

---
+++ src/modules/apiTvmaze.js
@@ -0,0 +1,5 @@
+export function fetchMovies() {
+    return fetch('https://api.tvmaze.com/search/shows?q=star%20wars')
+      .then(response => response.json())
+      .catch(error => console.error('Error:', error));
+  }
