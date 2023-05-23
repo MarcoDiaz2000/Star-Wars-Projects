0e3910f3599046563cc65ede6433e41d32ae5548
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-05-23 03:59:14 -0600

    Display number of likes and add new like

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { getLikes, likeItem } from './modules/apiInvolvement.js';
 import './style.css';
 import createCard from './modules/card.js';
 import fetchMovies from './modules/apiTvmaze.js';
@@ -12,24 +13,37 @@ logoContainer.appendChild(imgElement);
 const movieContainer = document.querySelector('.movies');
 
 let likes = {};
+let userLikes = {};  // save likes
 
 fetchMovies().then((data) => {
-  data.forEach((item) => {
-    // Inicializar contador de likes para este item
-    likes[item.id] = 0;
-
-    const cardHTML = createCard(item.show);
-    movieContainer.innerHTML += cardHTML;
-  });
-
-  // Agregar evento de click a todos los botones de "like"
-  Array.from(document.querySelectorAll('.like button')).forEach(button => {
-    button.addEventListener('click', function() {
-      const itemId = this.dataset.id;
-      // Asegurarse de que haya un valor inicial para likes[itemId]
-      likes[itemId] = likes[itemId] ? likes[itemId] : 0;
-      likes[itemId]++;
-      document.querySelector(`#likes-${itemId}`).textContent = likes[itemId];
+  getLikes().then((likeData) => {
+    likeData.forEach((like) => {
+      likes[like.item_id] = like.likes;
     });
+
+    data.forEach((item) => {
+      const card = createCard(item.show, likes[item.show.id] || 0);
+
+      card.querySelector(`button[data-id="${item.show.id}"]`).addEventListener('click', function() {
+        const heartIcon = this.querySelector('i');
+        if (!userLikes[item.show.id]) {
+          likeItem(item.show.id).then(() => {  // increment counter likes
+            likes[item.show.id] = (likes[item.show.id] || 0) + 1;
+            card.querySelector(`#likes-${item.show.id}`).textContent = likes[item.show.id];
+            userLikes[item.show.id] = true;  // save user
+            heartIcon.classList.remove('far');
+            heartIcon.classList.add('fas');
+          }).catch((error) => {
+            console.error('Error liking item:', error);
+          });
+        } else {
+          alert('You have already liked this article.');
+        }
+    });
+
+      movieContainer.appendChild(card);
+    });
+  }).catch((error) => {
+    console.error('Error getting likes:', error);
   });
 });
