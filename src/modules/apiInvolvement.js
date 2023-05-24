a6cbde837aca7cb28171a876fff9eafa19e163b1
Bifabig <biftubig@gmail.com>
2023-05-24 12:16:52 +0300

    add a comments diplay

--- src/modules/apiInvolvement.js
+++ src/modules/apiInvolvement.js
@@ -28,19 +28,66 @@ function likeItem(itemId) {
 function getLikes() {
   const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes/`;
 
-  return fetch(url)
+  return (
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('The API request failed');
+        }
+        return response.text();
+      })
+      // If 'data' has a value, it parses to a JSON object. Otherwise, it returns an empty array.
+      .then((data) => (data ? JSON.parse(data) : []))
+      .catch((error) => {
+        console.error('Error:', error); // at the end of the Project, this line will be removed.
+        return [];
+      })
+  );
+}
+
+function getComment(itemId) {
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/comments?item_id=${itemId}`;
+
+  return (
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('The API request failed');
+        }
+        return response.text();
+      })
+      // If 'data' has a value, it parses to a JSON object. Otherwise, it returns an empty array.
+      .then((data) => (data ? JSON.parse(data) : []))
+      .catch((error) => {
+        console.error('Error:', error); // at the end of the Project, this line will be removed.
+        return [];
+      })
+  );
+}
+
+function addComment(itemId, username, comment) {
+  const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/comments/`;
+
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: itemId,
+      username,
+      comment,
+    }),
+  })
     .then((response) => {
       if (!response.ok) {
         throw new Error('The API request failed');
       }
-      return response.text();
+      return { success: true };
     })
-    // If 'data' has a value, it parses to a JSON object. Otherwise, it returns an empty array.
-    .then((data) => (data ? JSON.parse(data) : []))
     .catch((error) => {
       console.error('Error:', error); // at the end of the Project, this line will be removed.
-      return [];
     });
 }
 
-export { likeItem, getLikes };
+export { likeItem, getLikes, getComment, addComment };
--- src/modules/commentsPopup.js
+++ src/modules/commentsPopup.js
@@ -1,4 +1,6 @@
-function commentsPopup(data) {
+import { getComment, addComment } from './apiInvolvement.js';
+
+const commentsPopup = (data) => {
   const commentsBtn = document.querySelectorAll('.comments-btn');
   commentsBtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
@@ -14,21 +16,28 @@ function commentsPopup(data) {
           const popup = document.createElement('div');
           popup.className = 'popup';
           popup.innerHTML = `
-        <div class="popup-items">
-          <img src="${item.show.image.medium}" alt="${item.show.name}">
-          <h2>${item.show.name}</h2>
-          <div class="details">
-            <div class="details-item">
-              <li>Language: ${item.show.language}</li>
-              <li>Premiered: ${item.show.premiered}</li>
+          <div class="popup-items">
+            <img src="${item.show.image.medium}" alt="${item.show.name}">
+            <h2>${item.show.name}</h2>
+            <div class="details">
+              <div class="details-item">
+                <li>Language: ${item.show.language}</li>
+                <li>Premiered: ${item.show.premiered}</li>
+              </div>
+              <div class="details-item">
+                <li>Rating: ${item.show.rating.average}</li>
+                <li>Genre: ${item.show.genres}</li>
+              </div>
             </div>
-            <div class="details-item">
-              <li>Rating: ${item.show.rating.average}</li>
-              <li>Genre: ${item.show.genres}</li>
+            <div>
+              <h3>Comments</h3>
+              <div class="comment-list"></div>
+              
+              
             </div>
+            
+
           </div>
-          
-        </div>
         `;
           popup.appendChild(closeBtn);
           main.appendChild(popup);
@@ -38,10 +47,20 @@ function commentsPopup(data) {
           close.addEventListener('click', () => {
             body.removeChild(main);
           });
+
+          getComment(item.show.id).then((commentData) => {
+            commentData.forEach((mycomment) => {
+              const singleComment = document.createElement('p');
+
+              const commentList = document.querySelector('.comment-list');
+              singleComment.innerHTML = `${mycomment.creation_date} ${mycomment.username}: ${mycomment.comment}`;
+              commentList.appendChild(singleComment);
+            });
+          });
         }
       });
     });
   });
-}
+};
 
 export default commentsPopup;
