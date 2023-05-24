13da47aba08f0b1578f4167c084b737b90d89b9b
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-05-23 18:08:15 -0600

    Fix ESLint

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ fetchMovies().then((data) => {
         movieContainer.appendChild(card);
       });
       // Movies counter on home page
-      document.getElementById('cardCounter').textContent = data.length; 
+      document.getElementById('cardCounter').textContent = data.length;
       // Comments popup
       commentsPopup(data);
     })
