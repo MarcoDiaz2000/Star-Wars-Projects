60469faa7434aa03fd3763e3ad3e38e140dd827d
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-05-25 05:13:26 -0600

    Fix ESLint

--- src/modules/mobileMenu.js
+++ src/modules/mobileMenu.js
@@ -1,13 +1,13 @@
 function mobileMenu() {
-    document.getElementById('menu-toggle').addEventListener('click', function () {
-      const nav = document.getElementById('nav');
-      const navigationLeft = getComputedStyle(nav).getPropertyValue('left');
-      if (navigationLeft === '0px') {
-        nav.style.left = '-100%';
-      } else {
-        nav.style.left = '0px';
-      }
-    });
-  }
+  document.getElementById('menu-toggle').addEventListener('click', function () {
+    const nav = document.getElementById('nav');
+    const navigationLeft = getComputedStyle(nav).getPropertyValue('left');
+    if (navigationLeft === '0px') {
+      nav.style.left = '-100%';
+    } else {
+      nav.style.left = '0px';
+    }
+  });
+}
 
-  export default mobileMenu;
+export default mobileMenu;
